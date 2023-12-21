1fbe3cb6e162f32ff8d5d702db619939bf0c5ea5
Saifulkhandaker <khsaiful67@gmail.com>
2023-12-21 22:36:26 +0600

    contact page added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,86 @@
-import React from 'react';
+import { useRef } from "react";
+import { FaCheckCircle } from "react-icons/fa";
+import Swal from "sweetalert2";
+import contact from "../../assets/images/contact.png";
 
 const Contact = () => {
+
+    const formRef = useRef();
+
+    const handlemessage = (e) => {
+      e.preventDefault();
+      Swal.fire({
+        position: "top-center",
+        icon: "success",
+        title: "Message Sent Successfully..!",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      
+       // Reset the form
+       formRef.current.reset();
+    }
+
     return (
         <div>
-            this is contact page
+            <div className='mt-16 w-[80%] mx-auto pb-10 lg:flex justify-between'>
+            <div className="space-y-5">
+                <h1 className='text-3xl text-[#3D474D] font-mono font-bold'>Contact <br /> Get In Touch.</h1>
+                <p className='text-xl text-[#3D474D] font-medium'>Tell us how we can help you succeed. We will:</p>
+                <p className="flex text-xl items-center gap-6 "><FaCheckCircle />Help increase your productivity.</p>
+                <p className="flex text-xl items-center gap-6 "><FaCheckCircle />Create a price and license plan best suited to your needs.</p>
+                <p className="flex text-xl items-center gap-6 "><FaCheckCircle />Introduce MeisterTask and explain its value to your business.</p>
+            </div>
+            <div className="lg:mt-0  mt-10 ">
+            <form onSubmit={handlemessage} ref={formRef}>
+              <div className="flex gap-5">
+                <input
+                  type="text"
+                  required
+                  placeholder="Enter Name"
+                  className=" w-full bg-[#F4F5F8] px-10 py-7 input  rounded-none"
+                />
+                <input
+                  type="email"
+                  required
+                  placeholder="Enter Email"
+                  className=" w-full bg-[#F4F5F8] px-10 py-7 input  rounded-none"
+                />
+              </div>
+              <div className="flex gap-5 mt-5">
+                <input
+                  type="text"
+                  required
+                  placeholder="Enter Subject"
+                  className=" w-full bg-[#F4F5F8] px-10 py-7 input  rounded-none"
+                />
+                <input
+                  type="number"
+                  placeholder="Enter Phone"
+                  required
+                  className=" w-full bg-[#F4F5F8] px-10 py-7 input  rounded-none"
+                />
+              </div>
+              <div className="mt-5">
+                <textarea
+                  required
+                  className="bg-[#F4F5F8] w-full px-10 py-7"
+                  placeholder="Enter Message"
+                  name=""
+                  id=""
+                  rows="5"
+                ></textarea>
+              </div>
+              <div className="mt-4">
+                <button className="hover:bg-[#8EA3E7] hover:text-white py-4 px-5 border rounded-none text-white font-medium bg-[#00AAFF] border-[#8EA3E7]">
+                  Send Message
+                </button>
+              </div>
+          </form>
+            </div>
+            
+        </div>
+        <img className="w-[80%] mx-auto my-10" src={contact} alt="" />
         </div>
     );
 };
